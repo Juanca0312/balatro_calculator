ecf51d07c190898bac58a194cef20f9bd0f80999
Juan Carlos Hernandez Castillo <juancarlos.hernandezcastillo+pacifico@emeal.nttdata.com>
2025-07-01 17:16:16 -0500

    feat: add card entity

---
+++ lib/core/entities/deck_card.dart
@@ -0,0 +1,21 @@
+import 'package:balatro_calculator/core/enums/card_rank.dart';
+import 'package:balatro_calculator/core/enums/card_suit.dart';
+
+class DeckCard {
+  final CardSuit suit;
+  final CardRank rank;
+  final String assetPath;
+  bool isEnabled;
+
+  DeckCard({
+    required this.suit,
+    required this.rank,
+    required this.assetPath,
+    this.isEnabled = true,
+  });
+
+  factory DeckCard.create(CardSuit suit, CardRank rank) {
+    final path = 'cards/${rank.symbol}_${suit.name}.png';
+    return DeckCard(suit: suit, rank: rank, assetPath: path);
+  }
+}
---
+++ lib/core/enums/card_suit.dart
@@ -0,0 +1,9 @@
+enum CardSuit {
+  hearts('hearts'),
+  diamonds('diamonds'),
+  clubs('clubs'),
+  spades('spades');
+
+  final String name;
+  const CardSuit(this.name);
+}
